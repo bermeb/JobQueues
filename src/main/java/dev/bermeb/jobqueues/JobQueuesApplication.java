8a3b3da26b2cbab5e0962685df4d5fef5f6a1f84
Bernhard <bernhard@bermeb.dev>
2026-06-27 22:24:12 +0200

    Init Project

---
+++ src/main/java/dev/bermeb/jobqueues/JobQueuesApplication.java
@@ -0,0 +1,13 @@
+package dev.bermeb.jobqueues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobQueuesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobQueuesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/bermeb/jobqueues/JobQueuesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bermeb.jobqueues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobQueuesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
